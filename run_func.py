64dc04af935d0fe5e92653e3d4444130455399c5
raiden098 <000tri@gmail.com>
2016-12-24 10:45:14 -0500

    added sudo process

--- run_func.py
+++ run_func.py
@@ -2,9 +2,17 @@
 
 #simple function to execute a command using a subprocess
 import subprocess
+import getpass
 
 def run_me(cmd):
    p1 = subprocess.Popen(cmd)
    (result,err) = p1.communicate()
 
-run_me("ls")
+def run_as_root(cmd):
+   user = getpass.getuser()
+   print "will run",cmd,"as root..."
+   #password = getpass.getpass("pass:")
+   p1 = subprocess.Popen(['sudo'] + cmd)
+   (result,err) = p1.communicate()
+
+run_as_root(['cat','/etc/passwd'])
