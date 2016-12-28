40a4e88240bd646bfda7e78e79aebb1c7cfcfeda
raiden098 <000tri@gmail.com>
2016-12-27 20:00:57 -0500

    updated

--- run_func.py
+++ run_func.py
@@ -5,14 +5,15 @@ import subprocess
 import getpass
 
 def run_me(cmd):
-   p1 = subprocess.Popen(cmd)
+   p1 = subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=False)
    (result,err) = p1.communicate()
+   print result
 
 def run_as_root(cmd):
    user = getpass.getuser()
    print "will run",cmd,"as root..."
    #password = getpass.getpass("pass:")
-   p1 = subprocess.Popen(['sudo'] + cmd)
+   p1 = subprocess.Popen(['sudo'] + cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=False)
    (result,err) = p1.communicate()
 
-run_as_root(['cat','/etc/passwd'])
+run_me(['cat','/etc/passwd'])
